3e88d5f48d9e345e67266b1bc5f45b18e0e7478b
Alex Jeffery <apjeffery136@googlemail.com>
2016-12-12 13:49:01 +0200

    added a c preprocessor header that implements a small library for LaT code.

---
+++ lat_stdlib.h
@@ -0,0 +1,34 @@
+// The Lambdas and Trees Standard Library
+// To use, include this file in source code and run the c preprocessor with
+//
+//     cpp -P infile -o outfile
+
+#ifndef stdlib
+#define stdlib
+
+// ---------------------------------
+// Boolean logic
+// ---------------------------------
+
+#define true (nil.nil)
+#define false nil
+
+#define and ( | a -> | b -> \
+    if a then \
+        if b then true else false end \
+    else \
+        false \
+    end \
+)
+
+#define or ( | a -> | b -> \
+    if a then \
+        true \
+    else \
+        if b then true else false end \
+    end \
+)
+
+#define not ( | a -> if a then false else a end )
+
+#endif
