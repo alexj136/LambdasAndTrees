77cbfca3f3e5ac7c9a5adc4d1a6e1f59dfcdc6f5
Alex Jeffery <apjeffery136@googlemail.com>
2016-12-16 20:47:03 +0200

    Util.Result is now a Test.QuickCheck.Testable.

--- src/test/TestUtil.hs
+++ src/test/TestUtil.hs
@@ -3,11 +3,12 @@ module TestUtil where
 import qualified Util as U
 import SugarSyntax
 import Types
-import Test.QuickCheck
+import Test.QuickCheck (Testable, property)
+import Test.QuickCheck.Property (failed, reason)
 
 instance Testable t => Testable (U.Result t) where
     property (U.Success e) = property e
-    property (U.Error   s) = property False
+    property (U.Error   s) = property $ failed { reason = s }
 
 instance Testable Type where
     property _ = property True
