df32872d6bbb156d2c29ee09dcd512ade19b3f30
Alex Jeffery <apjeffery136@googlemail.com>
2018-05-07 10:07:15 +0100

    Parser testing work

--- src/test/TestParser.hs
+++ src/test/TestParser.hs
@@ -7,5 +7,16 @@ import Lexer
 import Parser
 import SugarSyntax
 
+import qualified Data.Map as M
+
+parseStr :: String -> Result (M.Map Name String, Name, Term)
+parseStr progText = do
+    (names, nextName, tokens) <- scan progText
+    ast <- parse tokens
+    return (swap names, nextName, ast)
+
 tests :: [Test]
 tests = []
+
+p1 :: Test
+p1 = Test "Simple expression 1" $ True
